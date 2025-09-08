ca431e6bb4adb282092dbe3713209f38b1b4b8f2
Faisal Hakim <faisalhakim47@gmail.com>
2025-09-08 09:10:33 +0700

    fix(repo): injectable repo

--- src/shared/repositories/cryptogadai.repository.ts
+++ src/shared/repositories/cryptogadai.repository.ts
@@ -1,6 +1,9 @@
+import { Injectable } from '@nestjs/common';
+
 import { FinanceRepository } from './finance.repository';
 
 /**
  * CryptogadaiRepository <- AdminRepository <- LoanRepository <- FinanceRepository <- UserRepository <- DatabaseRepository
  */
+@Injectable()
 export abstract class CryptogadaiRepository extends FinanceRepository {}
